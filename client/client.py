0bc8df4a7e37a95e86d13e32996a273b0dd160ca
KC Erb <iamkcerb@gmail.com>
2023-05-12 14:17:42 -0400

    remove dead space in client.py

--- client/client.py
+++ client/client.py
@@ -5,7 +5,6 @@ capnp.remove_import_hook()
 path_to_go = os.path.expanduser('~') + "/go/src/capnproto.org/go/capnp/std/"
 hello_world_capnp = capnp.load(file_name="../hello_world.capnp", imports=[path_to_go])
 
-
 # hello_world_capnp = capnp.load('../hello_world.capnp')
 client = capnp.TwoPartyClient('localhost:8080')
 greeter = client.bootstrap().cast_as(hello_world_capnp.Greeter)
