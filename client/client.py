870f1ff1db58171fce34820f055009db03aeab3c
KC Erb <iamkcerb@gmail.com>
2023-05-12 14:18:22 -0400

    improve comment in client.py

--- client/client.py
+++ client/client.py
@@ -5,7 +5,9 @@ capnp.remove_import_hook()
 path_to_go = os.path.expanduser('~') + "/go/src/capnproto.org/go/capnp/std/"
 hello_world_capnp = capnp.load(file_name="../hello_world.capnp", imports=[path_to_go])
 
+# Or, if you used a full path ref in the .capnp, you can just do:
 # hello_world_capnp = capnp.load('../hello_world.capnp')
+
 client = capnp.TwoPartyClient('localhost:8080')
 greeter = client.bootstrap().cast_as(hello_world_capnp.Greeter)
 
